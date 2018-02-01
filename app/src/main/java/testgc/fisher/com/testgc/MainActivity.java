8e6f375203a122f34b49c0cfabe5a8193a1d078c
Mohammad Hasan <saffari21@gmail.com>
2018-02-01 20:56:47 +0330

    Rejected commit: Default

---
+++ app/src/main/java/testgc/fisher/com/testgc/MainActivity.java
@@ -0,0 +1,21 @@
+package testgc.fisher.com.testgc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et;
+    private EditText et2;
+    private EditText et3;
+    private EditText et4;
+    private EditText et5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
