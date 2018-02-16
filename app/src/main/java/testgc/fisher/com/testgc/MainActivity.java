e738d21843b71334fbf80aeda33388fbdf6b812d
Mohammad Hasan <saffari21@gmail.com>
2018-02-16 12:59:30 +0330

    third commit

--- app/src/main/java/testgc/fisher/com/testgc/MainActivity.java
+++ app/src/main/java/testgc/fisher/com/testgc/MainActivity.java
@@ -2,21 +2,48 @@ package testgc.fisher.com.testgc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+
+import io.github.kbiakov.codeview.CodeView;
+import io.github.kbiakov.codeview.adapters.Options;
+import io.github.kbiakov.codeview.classifier.CodeProcessor;
+import io.github.kbiakov.codeview.highlight.ColorTheme;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et;
-    private EditText et2;
-    private EditText et3;
-    private EditText et4;
-    private EditText et5;
-    private EditText et6;
+    private CodeView codeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        CodeProcessor.init(this);
+        codeView = (CodeView) findViewById(R.id.code_view);
+
+//        codeView.setCode(getString(R.string.listing_py));
+
+        codeView.setOptions(Options.Default.get(this)
+                .withLanguage("java")
+                .withCode(R.string.listing_py)
+                .withTheme(ColorTheme.SOLARIZED_LIGHT));
+
+        disable(codeView);
+
+    }
+
+
+    private static void disable(ViewGroup layout) {
+        layout.setEnabled(false);
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View child = layout.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                disable((ViewGroup) child);
+            } else {
+                child.setEnabled(false);
+            }
+        }
     }
 }
